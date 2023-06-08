0e5a245937f9a4cc635bfd904b751c4004a24ec1
Chris Wessels <chrisw@nashuacapetown.co.za>
2023-06-08 08:57:51 +0200

    name change

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -5,7 +5,7 @@ const Dashboard = () => {
   return (
     <Box m="20px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Header title="DASHBOARD" subtitle="Welcome to the modern dashboard" />
+        <Header title="MODERN DASHBOARD" subtitle="Welcome to the modern dashboard" />
       </Box>
     </Box>
   );
